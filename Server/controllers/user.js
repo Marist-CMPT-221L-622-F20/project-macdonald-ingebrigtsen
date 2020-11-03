3902e64b5f540b6728232b9fe04d4bd9cdc429f2
“C-MacD” <colin.macdonald1@marist.edu>
2020-11-03 12:45:56 -0500

    Created area controller and routes Tested it and it works. Some improvements to area model.

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -36,8 +36,6 @@ exports.updateUser = (req, res) => {
 
     if (!req.body) // request must contain data
         return res.status(400).send("Missing user data");
-    if (!req.body.name) // request data must contain name attribute
-        return res.status(400).send("Must specify the user name");
 
     const u = Usr.users[req.params.id];
     if (!u)  // did we find a matching user?
@@ -48,7 +46,6 @@ exports.updateUser = (req, res) => {
             u[prop] = req.body[prop];
     }
 
-    // s.name = req.body.name;
     res.sendStatus(204);
 };
 
--- Server/models/area.js
+++ Server/models/area.js
@@ -1,21 +1,27 @@
-/* area model represents a user
+/* area model represents an area
  * having the following properties:
+ *   - ID (an integer)
  *   - name (a String)
  *   - sublocation (a String)
+ *   - description (a String)
  */
 
-// Constructor for instances of the area model
-function Area(location, sublocation) {
+let nextID = 0;
 
+// Constructor for instances of the area model
+function Area(location, sublocation, description) {
+
+    this.ID = nextID++;
     this.location = location;
     this.sublocation = sublocation;
+    this.description = description ? description : "";
 
 }
 
 exports.Area = Area
 exports.areas = [
-    new Area("Lanes","Top"),
-    new Area("Lanes","Mid"),
-    new Area("Lanes","Bot"),
-    new Area("Jungle","Redside")
+    new Area("Lane", "Top"),
+    new Area("Lane", "Mid"),
+    new Area("Lane", "Bot"),
+    new Area("Jungle", "Redside")
 ];
